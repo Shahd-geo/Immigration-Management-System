5f453623208f014588d7104414c462c50918f8bd
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 12:38:25 +0400

    Autowired

--- immigration-system/src/main/java/com/shahd/immigration_system/service/OfficerService.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/service/OfficerService.java
@@ -1,10 +1,14 @@
 package com.shahd.immigration_system.service;
 
 import com.shahd.immigration_system.entity.ImmigrationOfficer;
+import com.shahd.immigration_system.repository.OfficerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OfficerService {
+    @Autowired
+    private OfficerRepository officerRepository;
 
 
     public ImmigrationOfficer promoteOfficer(Long officerId, String newRank, int newClearanceLevel){
