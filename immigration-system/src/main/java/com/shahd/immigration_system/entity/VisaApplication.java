d89ca4205aa839045c655f57c3b04ea265446fa5
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 10:08:22 +0400

    ADD FIELDS

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/VisaApplication.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/VisaApplication.java
@@ -1,20 +1,24 @@
 package com.shahd.immigration_system.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VisaApplication {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
     private String visaType;
-
     private String status;
-
     private String officerNotes;
 
 }
