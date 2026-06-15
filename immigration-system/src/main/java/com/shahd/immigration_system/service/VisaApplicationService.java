c23b4f6e1d1a77d59020721a294b150022af0278
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 14:45:23 +0400

    CREATE submitApplication METHOD

--- immigration-system/src/main/java/com/shahd/immigration_system/service/VisaApplicationService.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/service/VisaApplicationService.java
@@ -1,5 +1,6 @@
 package com.shahd.immigration_system.service;
 
+import com.shahd.immigration_system.entity.VisaApplication;
 import com.shahd.immigration_system.repository.ApplicantRepository;
 import com.shahd.immigration_system.repository.VisaApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,4 +13,6 @@ public class VisaApplicationService {
 
     @Autowired
     private ApplicantRepository applicantRepository;
+
+    public VisaApplication submitApplication(Long applicantId, String visaType) {
 }
