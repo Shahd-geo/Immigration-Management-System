5c79ac12119a10425a53ad4da7915d64b18f7ede
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 09:53:12 +0400

    IMPLEMENT GETTER AND SETTER

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/AsylumSeeker.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/AsylumSeeker.java
@@ -1,8 +1,14 @@
 package com.shahd.immigration_system.entity;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AsylumSeeker extends  Applicant {
     private String countryOfOrigin;
     private String sponsorOrganization;
