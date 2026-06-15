fd736e064a54d47a4e080c7e9ea5055334e53586
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 10:09:35 +0400

    ADD FIELDS

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/Interview.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/Interview.java
@@ -1,4 +1,12 @@
 package com.shahd.immigration_system.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Interview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
