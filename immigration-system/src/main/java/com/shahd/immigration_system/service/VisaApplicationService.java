8f923c0a96d13b935b90cddd3f40e2726bc96236
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 14:41:55 +0400

    add injection

--- immigration-system/src/main/java/com/shahd/immigration_system/service/VisaApplicationService.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/service/VisaApplicationService.java
@@ -1,7 +1,15 @@
 package com.shahd.immigration_system.service;
 
+import com.shahd.immigration_system.repository.ApplicantRepository;
+import com.shahd.immigration_system.repository.VisaApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VisaApplicationService {
+    @Autowired
+    private VisaApplicationRepository visaApplicationRepository;
+
+    @Autowired
+    private ApplicantRepository applicantRepository;
 }
