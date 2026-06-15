d9625772b2a10f9d92c74b5d72899d0e3334ab7f
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 12:16:33 +0400

    create OfficerService class

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/Applicant.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/Applicant.java
@@ -16,9 +16,7 @@ public class Applicant extends Person{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String passportNumber;
-
     private String nationality;
-
     private boolean criminalRecord;
 
     @OneToMany
