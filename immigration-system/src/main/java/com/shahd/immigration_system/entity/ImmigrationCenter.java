2e7f3ee681f7de5bc2d8e4e58a1a308534c55337
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 10:16:37 +0400

    ADD RELATION

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationCenter.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationCenter.java
@@ -1,12 +1,11 @@
 package com.shahd.immigration_system.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -17,12 +16,10 @@ public class ImmigrationCenter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     private String locationCountry;
-
     private String type;
-
     private int dailyCapacity;
+    @OneToMany(mappedBy = "center")
+    private List<ImmigrationOfficer> officers;
 }
