3c5e8f7c4d5658a924456105e32984b1a9e31a9a
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 10:04:09 +0400

    IMPLEMENT GETTER AND SETTER

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationCenter.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationCenter.java
@@ -1,9 +1,17 @@
 package com.shahd.immigration_system.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImmigrationCenter {
 
     @Id
