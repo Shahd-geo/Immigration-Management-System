d5344c108a35cfbec5ed451a61011c787c914b36
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 12:35:14 +0400

    create promoteOfficer method

--- immigration-system/src/main/java/com/shahd/immigration_system/service/OfficerService.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/service/OfficerService.java
@@ -1,4 +1,11 @@
 package com.shahd.immigration_system.service;
 
+import com.shahd.immigration_system.entity.ImmigrationOfficer;
+
 public class OfficerService {
+
+
+    public ImmigrationOfficer promoteOfficer(Long officerId, String newRank, int newClearanceLevel){
+
+    }
 }
