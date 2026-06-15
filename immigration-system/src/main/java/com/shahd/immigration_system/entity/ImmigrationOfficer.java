f4e1bbbd2573007e8f3cbff72954ffd085cff376
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 09:58:50 +0400

    CREATE BorderControlOfficer CLASS

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationOfficer.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationOfficer.java
@@ -1,9 +1,18 @@
 package com.shahd.immigration_system.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImmigrationOfficer  extends Person{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
