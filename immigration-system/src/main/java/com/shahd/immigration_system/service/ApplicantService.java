06b403c27519e66a17e464a9ef31dc2d125fc519
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 11:15:11 +0400

    add frist method

--- immigration-system/src/main/java/com/shahd/immigration_system/service/ApplicantService.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/service/ApplicantService.java
@@ -1,5 +1,6 @@
 package com.shahd.immigration_system.service;
 
+import com.shahd.immigration_system.entity.Applicant;
 import com.shahd.immigration_system.repository.ApplicantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,7 @@ public class ApplicantService {
     @Autowired
     private ApplicantRepository applicantRepository;
 
+    public Applicant saveApplicant(Applicant applicant){
+        return applicantRepository.save(applicant);
     }
 }
