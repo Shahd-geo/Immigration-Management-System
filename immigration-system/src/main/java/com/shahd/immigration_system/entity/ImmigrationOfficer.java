af8fd1fe53acad029f4b96938d72ee70bb4a7a24
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 09:55:51 +0400

    add fields

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationOfficer.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationOfficer.java
@@ -1,4 +1,11 @@
 package com.shahd.immigration_system.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class ImmigrationOfficer  extends Person{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
