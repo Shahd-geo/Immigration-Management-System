efbb145964d495ad896319c449f38e5dc1a0ce02
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 20:21:21 +0400

    CREATE ImmigrationException class

---
+++ immigration-system/src/main/java/com/shahd/immigration_system/exception/ImmigrationException.java
@@ -0,0 +1,29 @@
+package com.shahd.immigration_system.exception;
+import org.springframework.http.HttpStatus;
+
+public class ImmigrationException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public ImmigrationException(String message) {
+        super(message);
+        this.status = HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+    public ImmigrationException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public static ImmigrationException notFound(String message) {
+        return new ImmigrationException(HttpStatus.NOT_FOUND, message);
+    }
+
+    public static ImmigrationException badRequest(String message) {
+        return new ImmigrationException(HttpStatus.BAD_REQUEST, message);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
