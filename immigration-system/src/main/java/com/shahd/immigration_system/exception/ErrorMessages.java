ae854a481f94b4573fb4e265fec3da70f2d33123
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 20:22:52 +0400

    CREATE ErrorMessages class

---
+++ immigration-system/src/main/java/com/shahd/immigration_system/exception/ErrorMessages.java
@@ -0,0 +1,9 @@
+package com.shahd.immigration_system.exception;
+
+public class ErrorMessages {
+    public static final String APPLICANT_NOT_FOUND = "Applicant not found";
+    public static final String OFFICER_NOT_FOUND = "Officer not found";
+    public static final String CENTER_NOT_FOUND = "Center not found";
+    public static final String VISA_NOT_FOUND = "Visa application not found";
+    public static final String INTERVIEW_NOT_FOUND = "Interview not found";
+}
