9db955971a42c43c112e21a9e45fc8909c622a4a
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 15:23:05 +0400

    ADD INJECTION

--- immigration-system/src/main/java/com/shahd/immigration_system/service/InterviewService.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/service/InterviewService.java
@@ -1,7 +1,19 @@
 package com.shahd.immigration_system.service;
 
+import com.shahd.immigration_system.repository.ApplicantRepository;
+import com.shahd.immigration_system.repository.InterviewRepository;
+import com.shahd.immigration_system.repository.OfficerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InterviewService {
+    @Autowired
+    private InterviewRepository interviewRepository;
+
+    @Autowired
+    private ApplicantRepository applicantRepository;
+
+    @Autowired
+    private OfficerRepository officerRepository;
 }
