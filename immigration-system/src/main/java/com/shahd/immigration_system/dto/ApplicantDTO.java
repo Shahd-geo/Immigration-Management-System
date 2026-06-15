9f9d0f1127403dd7962d3e5196ddbe081ed3a660
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 21:32:18 +0400

    create ApplicantDTO ؤ

--- immigration-system/src/main/java/com/shahd/immigration_system/dto/ApplicantDTO.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/dto/ApplicantDTO.java
@@ -1,4 +1,42 @@
 package com.shahd.immigration_system.dto;
+import com.shahd.immigration_system.entity.Applicant;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class ApplicantDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String passportNumber;
+    private String nationality;
+    private boolean criminalRecord;
+
+    public static ApplicantDTO convertToDTO(Applicant entity) {
+        ApplicantDTO dto = new ApplicantDTO();
+        dto.setId(entity.getId());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setPassportNumber(entity.getPassportNumber());
+        dto.setNationality(entity.getNationality());
+        dto.setCriminalRecord(entity.isCriminalRecord());
+
+        return dto;
+    }
+
+    public static List<ApplicantDTO> convertToDTO(List<Applicant> entities) {
+
+        List<ApplicantDTO> dtos = new ArrayList<>();
+
+        for (Applicant entity : entities) {
+            dtos.add(convertToDTO(entity));
+        }
+
+        return dtos;
+    }
 }
+
