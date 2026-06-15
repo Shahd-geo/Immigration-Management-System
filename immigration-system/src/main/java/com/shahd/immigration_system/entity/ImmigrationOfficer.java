9a7b76ab5db297d04ae664a5a347462815942542
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 09:56:27 +0400

    add fields

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationOfficer.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/ImmigrationOfficer.java
@@ -8,4 +8,12 @@ public class ImmigrationOfficer  extends Person{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String badgeNumber;
+
+    private String rank;
+
+    private int clearanceLevel;
+
+    private boolean active;
 }
