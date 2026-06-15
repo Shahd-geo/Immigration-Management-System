362aad8e95a8681526cca5f457d7b9339994a413
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 10:01:34 +0400

    CREATE ImmigrationCenter CLASS

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/BorderControlOfficer.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/BorderControlOfficer.java
@@ -1,4 +1,16 @@
 package com.shahd.immigration_system.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BorderControlOfficer extends ImmigrationOfficer {
+    private String assignedCheckpoint;
+
+    private boolean k9UnitAssigned;
 }
