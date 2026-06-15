e722b27b3d30d05aa2e8a9cae5356684c3bcf9ad
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 09:23:25 +0400

    Initial Spring Boot project

---
+++ immigration-system/src/main/java/com/shahd/immigration_system/ImmigrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shahd.immigration_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmigrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmigrationSystemApplication.class, args);
+	}
+
+}
---
+++ immigration-system/src/test/java/com/shahd/immigration_system/ImmigrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahd.immigration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmigrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
