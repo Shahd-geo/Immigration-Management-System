ea7633b89c7bf8dc4e62ca4d66445d413073abe0
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 10:47:48 +0400

    add custom quary

--- immigration-system/src/main/java/com/shahd/immigration_system/repository/ApplicantRepository.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/repository/ApplicantRepository.java
@@ -3,5 +3,8 @@ package com.shahd.immigration_system.repository;
 import com.shahd.immigration_system.entity.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
+    List<Applicant> findByNationality(String nationality);
 }
