e63fbddccccc9e47b1272adc68d2429c0b2d4fb4
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 09:33:42 +0400

    IMPLEMENT GETTER AND SETTER

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/Person.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/Person.java
@@ -1,6 +1,15 @@
 package com.shahd.immigration_system.entity;
 
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 
 @MappedSuperclass
 public  abstract class Person {
