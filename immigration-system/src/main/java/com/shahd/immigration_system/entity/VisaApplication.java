1b58449b6f3187e5aa9b5281f3ae2549339d7dde
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 10:12:30 +0400

    ADD RELATION

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/VisaApplication.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/VisaApplication.java
@@ -1,9 +1,6 @@
 package com.shahd.immigration_system.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,5 +17,10 @@ public class VisaApplication {
     private String visaType;
     private String status;
     private String officerNotes;
+
+    @ManyToOne
+    private Applicant applicant;
+    @ManyToOne
+    private ImmigrationOfficer handlingOfficer;
 
 }
