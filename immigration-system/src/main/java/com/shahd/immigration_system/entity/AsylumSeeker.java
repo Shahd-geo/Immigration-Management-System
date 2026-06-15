72d3a9cd17f8363bab347c9089d8b29c6de3143d
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 09:52:15 +0400

    ADD FIELDS

--- immigration-system/src/main/java/com/shahd/immigration_system/entity/AsylumSeeker.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/entity/AsylumSeeker.java
@@ -1,4 +1,9 @@
 package com.shahd.immigration_system.entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class AsylumSeeker extends  Applicant {
+    private String countryOfOrigin;
+    private String sponsorOrganization;
 }
