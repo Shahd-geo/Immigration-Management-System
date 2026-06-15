6ff74c12158f6e59e488cf92739097023b73b820
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 11:13:13 +0400

    create Injection

--- immigration-system/src/main/java/com/shahd/immigration_system/service/ApplicantService.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/service/ApplicantService.java
@@ -1,7 +1,14 @@
 package com.shahd.immigration_system.service;
 
+import com.shahd.immigration_system.repository.ApplicantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicantService {
+
+    @Autowired
+    private ApplicantRepository applicantRepository;
+
+    }
 }
