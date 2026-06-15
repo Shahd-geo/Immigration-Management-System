57367bf14c1c9c168650b57155c9915750b4844d
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 20:28:35 +0400

    add RestControllerAdvice

--- immigration-system/src/main/java/com/shahd/immigration_system/exception/GlobalExceptionHandler.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/exception/GlobalExceptionHandler.java
@@ -1,4 +1,7 @@
 package com.shahd.immigration_system.exception;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 }
