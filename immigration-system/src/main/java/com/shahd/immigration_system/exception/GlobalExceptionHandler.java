857d8e4bf30047f3a36d10aca47a11e805af4c44
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 20:32:47 +0400

    Handle GenericException

--- immigration-system/src/main/java/com/shahd/immigration_system/exception/GlobalExceptionHandler.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/exception/GlobalExceptionHandler.java
@@ -1,7 +1,27 @@
 package com.shahd.immigration_system.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(GenericException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(
+            GenericException ex,
+            WebRequest request) {
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND,
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=", "")
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
 }
