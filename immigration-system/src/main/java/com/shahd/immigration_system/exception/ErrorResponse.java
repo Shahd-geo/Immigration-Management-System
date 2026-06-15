86ce17760fc80f68d4ce5f6fb69830821cc9d183
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 15:50:28 +0400

    CREATE ErrorResponse CLASS

--- immigration-system/src/main/java/com/shahd/immigration_system/exception/ErrorResponse.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/exception/ErrorResponse.java
@@ -1,4 +1,17 @@
 package com.shahd.immigration_system.exception;
 
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
 public class ErrorResponse {
+    private Date timestamp;
+    private int statusCode;
+    private HttpStatus status;
+    private String error;
+    private String message;
+    private String path;
+
+
+
 }
