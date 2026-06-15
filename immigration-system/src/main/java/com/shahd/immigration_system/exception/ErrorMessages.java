2d336c5a9e8f4060268e583494a986aff674f834
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-15 20:39:02 +0400

    add error massage

--- immigration-system/src/main/java/com/shahd/immigration_system/exception/ErrorMessages.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/exception/ErrorMessages.java
@@ -6,4 +6,7 @@ public class ErrorMessages {
     public static final String CENTER_NOT_FOUND = "Center not found";
     public static final String VISA_NOT_FOUND = "Visa application not found";
     public static final String INTERVIEW_NOT_FOUND = "Interview not found";
+    public static final String PASSPORT_REQUIRED = "Passport number is required";
+    public static final String FIRST_NAME_REQUIRED = "First name is required";
+    public static final String LAST_NAME_REQUIRED = "Last name is required";
 }
