bb8a3bff5c59216888db2715daa0f92872536312
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 12:41:03 +0400

    create an object from ImmigrationCenter

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/DatabaseSeedController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/DatabaseSeedController.java
@@ -1,5 +1,6 @@
 package com.shahd.immigration_system.controller;
 
+import com.shahd.immigration_system.entity.ImmigrationCenter;
 import com.shahd.immigration_system.repository.ApplicantRepository;
 import com.shahd.immigration_system.repository.CenterRepository;
 import com.shahd.immigration_system.repository.OfficerRepository;
@@ -17,6 +18,9 @@ public class DatabaseSeedController {
     private ApplicantRepository applicantRepository;
     @PostMapping("/api/seed")
     public String seedDatabase() {
+        // Centers
+        ImmigrationCenter center1 = new ImmigrationCenter();
+    }
 
 
 }
