cbd4fe13f9b3f4bb002549e6a737279c4911a4c0
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 08:41:22 +0400

    POST /api/applicants

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/ApplicantController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/ApplicantController.java
@@ -1,7 +1,11 @@
 package com.shahd.immigration_system.controller;
 
+import com.shahd.immigration_system.dto.ApplicantDTO;
+import com.shahd.immigration_system.entity.Applicant;
 import com.shahd.immigration_system.service.ApplicantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicantController {
     @Autowired
     private ApplicantService applicantService;
+
+    //POST /api/applicants
+    @PostMapping
+    public ApplicantDTO createApplicant(@RequestBody Applicant applicant) {
+
+        return ApplicantDTO.convertToDTO(applicantService.saveApplicant(applicant)
+        );
+    }
 
 }
