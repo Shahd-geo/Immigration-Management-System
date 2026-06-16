295c3c201146e309c425662a5e08c015b75b815c
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 11:13:21 +0400

    sechdual interview

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/InterviewController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/InterviewController.java
@@ -1,13 +1,21 @@
 package com.shahd.immigration_system.controller;
 
+import com.shahd.immigration_system.dto.InterviewDTO;
 import com.shahd.immigration_system.service.InterviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/interviews")
 public class InterviewController {
     @Autowired
     private InterviewService interviewService;
+
+    // POST /api/interviews/schedule/{applicantId}/{officerId}?date=2025-06-16
+    @PostMapping("/schedule/{applicantId}/{officerId}")
+    public InterviewDTO scheduleInterview(@PathVariable Long applicantId, @PathVariable Long officerId, @RequestParam String date) {
+        return InterviewDTO.convertToDTO(interviewService.scheduleInterview(applicantId, officerId, date)
+        );
+    }
+
 }
