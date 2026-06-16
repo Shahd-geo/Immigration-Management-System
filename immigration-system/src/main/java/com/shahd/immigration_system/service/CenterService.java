96912629b7a85be12b8e679cb91ea2a1839a86e1
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 08:21:13 +0400

    create CenterService to used in CenterController

--- immigration-system/src/main/java/com/shahd/immigration_system/service/CenterService.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/service/CenterService.java
@@ -1,4 +1,24 @@
 package com.shahd.immigration_system.service;
 
+import com.shahd.immigration_system.entity.ImmigrationCenter;
+import com.shahd.immigration_system.exception.ErrorMessages;
+import com.shahd.immigration_system.exception.ImmigrationException;
+import com.shahd.immigration_system.repository.CenterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CenterService {
+
+    @Autowired
+    private CenterRepository centerRepository;
+    //create new ImmigrationCenter
+    public ImmigrationCenter saveCenter(ImmigrationCenter center) {
+        return centerRepository.save(center);
+    }
+
+    public ImmigrationCenter getCenter(Long id) {
+        return centerRepository.findById(id)
+                .orElseThrow(() -> ImmigrationException.notFound(ErrorMessages.CENTER_NOT_FOUND));
+    }
 }
