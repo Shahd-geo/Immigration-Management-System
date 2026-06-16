6595703b4d4f556b228c46151e7219f628d28328
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 08:26:37 +0400

    add createCenter method

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/CenterController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/CenterController.java
@@ -1,7 +1,11 @@
 package com.shahd.immigration_system.controller;
 
+import com.shahd.immigration_system.dto.CenterDTO;
+import com.shahd.immigration_system.entity.ImmigrationCenter;
 import com.shahd.immigration_system.service.CenterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,11 @@ public class CenterController {
 
     @Autowired
     private CenterService centerService;
+    @PostMapping
+    public CenterDTO createCenter(@RequestBody ImmigrationCenter center) {
+
+        return CenterDTO.convertToDTO(centerService.saveCenter(center)
+        );
+    }
 
 }
