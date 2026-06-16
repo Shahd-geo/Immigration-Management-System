c09865225be28ba49567eca2c83098fbfd32ba5a
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 08:42:37 +0400

    //POST /api/applicants/asylum

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/ApplicantController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/ApplicantController.java
@@ -2,6 +2,7 @@ package com.shahd.immigration_system.controller;
 
 import com.shahd.immigration_system.dto.ApplicantDTO;
 import com.shahd.immigration_system.entity.Applicant;
+import com.shahd.immigration_system.entity.AsylumSeeker;
 import com.shahd.immigration_system.service.ApplicantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,13 @@ public class ApplicantController {
     public ApplicantDTO createApplicant(@RequestBody Applicant applicant) {
 
         return ApplicantDTO.convertToDTO(applicantService.saveApplicant(applicant)
+        );
+    }
+    //POST /api/applicants/asylum
+    @PostMapping("/asylum")
+    public ApplicantDTO createAsylumSeeker(@RequestBody AsylumSeeker seeker) {
+
+        return ApplicantDTO.convertToDTO(applicantService.saveApplicant(seeker)
         );
     }
 
