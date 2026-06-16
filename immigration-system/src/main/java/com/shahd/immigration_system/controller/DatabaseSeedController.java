10f1f2b09eab5b59f7792efc1515177ca72dc3fe
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 12:36:05 +0400

    Autowired CenterRepository

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/DatabaseSeedController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/DatabaseSeedController.java
@@ -1,7 +1,13 @@
 package com.shahd.immigration_system.controller;
 
+import com.shahd.immigration_system.repository.CenterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DatabaseSeedController {
+    @Autowired
+    private CenterRepository centerRepository;
+
 }
