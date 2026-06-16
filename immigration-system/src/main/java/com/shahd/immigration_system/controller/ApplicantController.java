7dcc694b35fe6b79c9c6c46377bcbdf501fe416d
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 08:40:06 +0400

    make injection for centerservicE

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/ApplicantController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/ApplicantController.java
@@ -1,9 +1,14 @@
 package com.shahd.immigration_system.controller;
 
+import com.shahd.immigration_system.service.ApplicantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/applicants")
 public class ApplicantController {
+    @Autowired
+    private ApplicantService applicantService;
+
 }
