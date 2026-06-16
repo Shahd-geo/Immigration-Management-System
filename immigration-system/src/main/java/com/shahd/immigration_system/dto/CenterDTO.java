b4567c11ad8174532e14f24b5c7bb099c9279088
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 07:43:24 +0400

    create CenterDTO

--- immigration-system/src/main/java/com/shahd/immigration_system/dto/CenterDTO.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/dto/CenterDTO.java
@@ -1,4 +1,39 @@
 package com.shahd.immigration_system.dto;
 
+import com.shahd.immigration_system.entity.ImmigrationCenter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class CenterDTO {
+
+    private Long id;
+    private String name;
+    private String locationCountry;
+    private String type;
+    private int dailyCapacity;
+
+    public static CenterDTO convertToDTO(ImmigrationCenter entity) {
+
+        CenterDTO dto = new CenterDTO();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        dto.setLocationCountry(entity.getLocationCountry());
+        dto.setType(entity.getType());
+        dto.setDailyCapacity(entity.getDailyCapacity());
+        return dto;
+    }
+
+    public static List<CenterDTO> convertToDTO(List<ImmigrationCenter> entities) {
+        List<CenterDTO> dtos = new ArrayList<>();
+        for (ImmigrationCenter entity : entities) {
+            dtos.add(convertToDTO(entity));
+        }
+
+        return dtos;
+    }
 }
