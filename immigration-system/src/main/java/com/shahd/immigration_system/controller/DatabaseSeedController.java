969e695526185bc077ec65fbaca4186325ca1bb9
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 12:49:28 +0400

    create an object from ImmigrationOfficer

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/DatabaseSeedController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/DatabaseSeedController.java
@@ -42,6 +42,7 @@ public class DatabaseSeedController {
         officer1.setClearanceLevel(2);
         officer1.setActive(true);
         officer1.setCenter(center1);
+        ImmigrationOfficer officer2 = new ImmigrationOfficer();
 
 
     }
