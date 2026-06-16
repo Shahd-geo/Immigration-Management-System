ade9c8d78a15238388447cfac9f5de1b102ceda2
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 08:37:25 +0400

    add RestController notaion

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/ApplicantController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/ApplicantController.java
@@ -1,4 +1,9 @@
 package com.shahd.immigration_system.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/applicants")
 public class ApplicantController {
 }
