424c2a06ccee81d7d17d9fc91472bbc2971b20c5
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 07:54:39 +0400

    add InterviewDTO with remove sensitive data

--- immigration-system/src/main/java/com/shahd/immigration_system/dto/InterviewDTO.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/dto/InterviewDTO.java
@@ -1,4 +1,35 @@
 package com.shahd.immigration_system.dto;
 
+import com.shahd.immigration_system.entity.Interview;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class InterviewDTO {
+    private Long id;
+    private String interviewDate;
+    private String status;
+    private String purpose;
+
+    public static InterviewDTO convertToDTO(Interview entity) {
+        InterviewDTO dto = new InterviewDTO();
+        dto.setId(entity.getId());
+        dto.setInterviewDate(entity.getInterviewDate());
+        dto.setStatus(entity.getStatus());
+        dto.setPurpose(entity.getPurpose());
+
+        return dto;
+    }
+
+    public static List<InterviewDTO> convertToDTO(List<Interview> entities) {
+        List<InterviewDTO> dtos = new ArrayList<>();
+        for (Interview entity : entities) {
+            dtos.add(convertToDTO(entity));
+        }
+
+        return dtos;
+    }
 }
