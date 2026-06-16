b528bbc0a23d638e879c424abe474692f98354d0
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 10:35:52 +0400

    POST /api/visas/submit/{applicantId}?type

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/VisaApplicationController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/VisaApplicationController.java
@@ -1,4 +1,20 @@
 package com.shahd.immigration_system.controller;
 
+import com.shahd.immigration_system.dto.VisaApplicationDTO;
+import com.shahd.immigration_system.service.VisaApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/visas")
 public class VisaApplicationController {
+    @Autowired
+    private VisaApplicationService visaApplicationService;
+
+    // POST /api/visas/submit/{applicantId}?type
+    @PostMapping("/submit/{applicantId}")
+    public VisaApplicationDTO submitVisa(@PathVariable Long applicantId, @RequestParam String type) {
+        return VisaApplicationDTO.convertToDTO(visaApplicationService.submitApplication(applicantId, type)
+        );
+    }
 }
