d933d0690b4fe6aef671606106a8cffee757fc1d
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-16 11:06:04 +0400

    add InterviewService

--- immigration-system/src/main/java/com/shahd/immigration_system/controller/InterviewController.java
+++ immigration-system/src/main/java/com/shahd/immigration_system/controller/InterviewController.java
@@ -1,9 +1,13 @@
 package com.shahd.immigration_system.controller;
 
+import com.shahd.immigration_system.service.InterviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/interviews")
 public class InterviewController {
+    @Autowired
+    private InterviewService interviewService;
 }
